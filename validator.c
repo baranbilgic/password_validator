caeaf78c4b7521e96f3b81b26119f60186913afc
Haluk Baran <75129280+baranbilgic@users.noreply.github.com>
2021-03-10 16:48:04 +0100

    Update validator.c

--- validator.c
+++ validator.c
@@ -9,21 +9,25 @@ int has_more_than_six(const char*password);
 int has_uppercase(const char*password);
 int has_number(const char*password);
 
-int main (){
+int main ()
+{
 
 	char password[100];
 	printf("Enter your password:\n");
 	scanf("%s", password);
 	
-	if(!has_more_than_six(password)){
+	if(!has_more_than_six(password))
+	{
 		printf("\nPassword has less than 6 characters. Exitting.\n\n");
 		exit(1);
 	}
-	if(!has_uppercase(password)){
+	if(!has_uppercase(password))
+	{
 		printf("\nPassword doesn't contain an uppercase letter. Exitting.\n\n");
 		exit(1);
 	}
-	if(!has_number(password)){
+	if(!has_number(password))
+	{
 		printf("\nPassword doesn't contain a number. Exitting.\n\n");
 		exit(1);
 	}
@@ -34,13 +38,15 @@ int main (){
 }
 
 
-int has_more_than_six(const char*password){
+int has_more_than_six(const char*password)
+{
 	int is_valid = false;
 	if( strlen(password) > 5) is_valid = true;
 	return is_valid;
 }
 
-int has_uppercase(const char*password){
+int has_uppercase(const char*password)
+{
 	int is_valid = false;
 	int i;
 	for( i = 0; i < strlen(password); ++i)
@@ -50,7 +56,8 @@ int has_uppercase(const char*password){
 	return is_valid;
 }
 
-int has_number(const char*password){
+int has_number(const char*password)
+{
 	int is_valid = false;
 	int i;
 	for( i = 0; i < strlen(password); ++i)
